4581e30015cff5ce02b7dbbacc54f87464c716f3
KevinZucca <kev.94@live.it>
2023-11-08 16:47:16 +0100

    feat: added function to get votes avg

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const collection = [
     title: "Inception",
     year: "2010",
     genre: ["Sci-Fi, Action"],
-    rating: "8.8",
+    rating: 8.8,
     type: "Movie",
   },
   {
     title: "Breaking Bad",
     year: "2008",
     genre: ["Crime, Drama, Thriller"],
-    rating: "9.5",
+    rating: 9.5,
     type: "Tv",
     seasons: 5,
   },
@@ -20,14 +20,14 @@ const collection = [
     title: "The Dark Knight",
     year: "2008",
     genre: ["Action, Crime, Drama"],
-    rating: "9.0",
+    rating: 9,
     type: "Movie",
   },
   {
     title: "Stranger Things",
     year: "2016",
     genre: ["Drama, Fantasy, Horror"],
-    rating: "8.7",
+    rating: 8.7,
     type: "Tv",
     seasons: 4,
   },
@@ -35,14 +35,14 @@ const collection = [
     title: "Pulp Fiction",
     year: "1994",
     genre: ["Crime, Drama"],
-    rating: "8.9",
+    rating: 8.9,
     type: "Movie",
   },
   {
     title: "Game of Thrones",
     year: "2011",
     genre: ["Action, Adventure, Drama"],
-    rating: "9.3",
+    rating: 9.3,
     type: "Tv",
     seasons: 8,
   },
@@ -90,4 +90,17 @@ const newArray = collection.map((el) => {
   }
 });
 
-console.log(newArray[5].toString());
+// functions to get votes avg from movies and tv series genres
+const genre = (array, genre) => {
+  const avg = [];
+  array.forEach((el) => {
+    if (el.genre.some((singleGenre) => singleGenre.includes(genre))) {
+      avg.push(el.rating);
+    }
+  });
+  const sum = parseInt(avg.reduce((a, b) => a + b));
+  return sum / avg.length;
+};
+
+const genreAvg = genre(newArray, "Action");
+console.log(genreAvg);
