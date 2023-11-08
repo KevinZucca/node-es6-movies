a2b289a327790ef0ea659e7645b63e4653c38c50
KevinZucca <kev.94@live.it>
2023-11-08 17:49:19 +0100

    feat: added getter

--- app.js
+++ app.js
@@ -50,6 +50,11 @@ const collection = [
 
 // creating a Movie and tv series classes
 class Movie {
+  #title;
+  #year;
+  #genre;
+  #rating;
+  #type;
   constructor(title, year, genre, rating, type) {
     this.title = title;
     this.year = year;
@@ -58,19 +63,44 @@ class Movie {
     this.type = type;
   }
 
+  getTitle() {
+    return this.#title;
+  }
+
+  getYear() {
+    return this.#year;
+  }
+
+  getGenre() {
+    return this.#genre;
+  }
+
+  getRating() {
+    return this.#rating;
+  }
+
+  getType() {
+    return this.#type;
+  }
+
   toString() {
-    return `${this.title} è un film di genere ${this.genre}. E' stato rilasciato nel ${this.year} ed ha un voto di ${this.rating}.`;
+    return `${this.getTitle()} è un film di genere ${this.getGenre()}. E' stato rilasciato nel ${this.getYear()} ed ha un voto di ${this.getRating()}.`;
   }
 }
 
 class TvSeries extends Movie {
+  #seasons;
   constructor(title, year, genre, rating, type, seasons) {
     super(title, year, genre, rating, type);
     this.seasons = seasons;
   }
 
+  getSeasons() {
+    return this.#seasons;
+  }
+
   toString() {
-    return `${this.title} è una serie tv di genere ${this.genre}. La prima stagione è stata rilasciata nel ${this.year} ed in totale sono state prodotte ${this.seasons} stagioni. Ha un voto di ${this.rating}.`;
+    return `${this.getTitle()} è una serie tv di genere ${this.getGenre()}. La prima stagione è stata rilasciata nel ${this.getYear()} ed in totale sono state prodotte ${this.getSeasons()} stagioni. Ha un voto di ${this.getRating()}.`;
   }
 }
 
